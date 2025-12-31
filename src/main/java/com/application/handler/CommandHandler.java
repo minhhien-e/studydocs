ddc39368da0ee575dffe88b243af54b812228fd0
lambaoduy <22130057@st.hcmuaf.edu.vn>
2026-01-01 01:43:49 +0700

    refactor: call api to notification sẻvice

--- src/main/java/com/application/handler/CommandHandler.java
+++
@@ -1,5 +0,0 @@
-package com.application.handler;
-public interface CommandHandler<C, R> {
-    R handle(C command);
-    Class<C> commandType();
-}
--- src/main/java/com/domain/command/GetAllUser.java
+++
@@ -1,7 +0,0 @@
-package com.domain.command;
-
-import lombok.Value;
-
-@Value(staticConstructor = "commandOf")
-public class GetAllUser implements UserCommand{
-}
